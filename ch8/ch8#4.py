90748b94f6c9c3625b5b5c15853c6c5d08caf652
jo5546fv <33111069+jo5546fv@users.noreply.github.com>
2017-11-07 16:18:01 -0600

    Update ch8#4.py

--- ch8/ch8#4.py
+++ ch8/ch8#4.py
@@ -1,8 +1,19 @@
 dic_1 = {'A':1,'B':2,'C':3}
 
-user = input('Enter A B or C and see the key for it\n')
+user = input('Enter A B or C and see the value for it\n').upper()
 
-print(dic_1[user])
+print('\nheres your value:',dic_1.get(user, 'N/A'))
+
+dic_2 = {'D': 3, 'E': 4, 'F': 5}
+
+print('\ntry our new and imporoved dictionary! with twice as many values!\n')
+user = input('Enter A, B, C, D, E, or F and see the value!\n').upper()
+
+dic_1.update(dic_2)
+
+print('\nheres your new and improved value:',dic_1.pop(user, 'N/A'))
+
+print('\nand heres the dictionary without your value:\n', dic_1)
 
 
 
