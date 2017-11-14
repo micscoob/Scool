16529a6bc74128c4f40f38d2b0b6f05c78f6ca24
nikolojedison <nikolojedison@fishallnite.com>
2017-11-14 13:23:22 -0600

    pupdate

--- CNAP/Random_Employee_Gen.py
+++ CNAP/Random_Employee_Gen.py
@@ -1,28 +1,27 @@
 import random
 import csv
 from faker import Faker
-fake = Faker()
-#List for each catagory
-first_nam = []
-last_nam = []
-ssn_list = []
-email_list = []
-username_list = []
-######Company Email####
-company = '@mycompany.com'
 
-def exportUsers(filename, listLength):
+fake = Faker('en_US')
+
+def genUsers(filename, listLength, company):
+    """Generate a set of fake users with sane-sounding fake data, and export them to a .CSV."""
+
     with open(filename, 'w') as filename:
+
         wr = csv.writer(filename, delimiter=",", quoting=csv.QUOTE_ALL)
+
         for i in range(listLength):
-            row = [] * listLength
+
+            row = [] * 6 # number of fields
             first = fake.first_name()
             last = fake.last_name()
             ssn = fake.ssn()
-            email = first + '.' + last + company
-            username = fake.user_name()
+            username = first.lower() + '.' + last.lower()
+            email = username + '@' + company + '.com'
+            phone = fake.phone_number()
 
-            row.append(first + ", " + last + ", " + ssn + ", " + email + ", " + username)
+            row.append(first + ',' + last + ',' + ssn + ',' + username + ',' + email + ',' + phone)
             wr.writerow(row)
 
-exportUsers('test_list_pls_ignore.csv', 10)
+genUsers('test_list_pls_ignore.csv', 10, 'contoso')
