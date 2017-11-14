22f6891e7bc75ae8b7580e61ccc2061fdcce37f8
micscoob <33203811+micscoob@users.noreply.github.com>
2017-11-14 09:47:34 -0600

    Update Random_Employee_Gen.py

--- CNAP/Random_Employee_Gen.py
+++ CNAP/Random_Employee_Gen.py
@@ -12,8 +12,7 @@ class Employee:
         self.pay = pay
         self.email = first + '.' + last + '@company.com'
 
-#emp_1 = Employee('Corey', 'Schafer', 50000)
-#emp_2 = Employee('Micah', 'Kryzer', 70000)
+
 
 emp_1 = Employee(random.choice(first_list), random.choice(last_list), random.choice(pay_list))
 emp_2 = Employee(random.choice(first_list), random.choice(last_list), random.choice(pay_list))
