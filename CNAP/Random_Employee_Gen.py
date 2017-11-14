72fe0836402c50fa9fdb37026aec81f4bd3da458
micscoob <33203811+micscoob@users.noreply.github.com>
2017-11-14 10:57:58 -0600

    Update Random_Employee_Gen.py

--- CNAP/Random_Employee_Gen.py
+++ CNAP/Random_Employee_Gen.py
@@ -1,10 +1,15 @@
 import random
 import csv
 from faker import Faker
-
-first_list = 'Sophia','Jackson','Flimingo'
-last_list = 'THOMPSON', 'ANDERSON'
-pay_list = 20000,40000,5000,6000,7000
+fake = Faker()
+#List for each catagory
+first_nam = []
+last_nam = []
+ssn_list = []
+email_list = []
+username_list = []
+######Company Email####
+company = '@mycompany.com'
 
 class Users:
 
@@ -20,3 +25,18 @@ def exportUsers(filename, listname):
         wr.writerow(listname)
 
 exportUsers('test_list_pls_ignore.csv', first_list)
+
+    for _ in range(100):
+    first = fake.first_name()
+    last = fake.last_name()
+    ssn = fake.ssn()
+    email = first + '.' + last + company
+    username = fake.user_name()
+    
+    first_nam.append(first)
+    last_nam.append(last)
+    ssn_list.append(ssn)
+    email_list.append(email)
+    username_list.append(username)
+
+    
